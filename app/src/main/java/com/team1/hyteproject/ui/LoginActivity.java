b9a0ad086ea7c9bc43aad57dab8d37ec8ab7c6c4
Samu <samuwahlroos29@gmail.com>
2021-02-26 19:22:22 +0200

    Login page without user information class. Now app starts with this page. Has few warnings but nothing major

---
+++ app/src/main/java/com/team1/hyteproject/ui/LoginActivity.java
@@ -0,0 +1,154 @@
+package com.team1.hyteproject.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.team1.hyteproject.HomeActivity;
+import com.team1.hyteproject.R;
+import com.team1.hyteproject.program.Credentials;
+
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+    private Button registerButton;
+    private TextView eAttemptsInfo;
+    private CheckBox eRememberMe;
+    private CheckBox showPassword;
+
+    boolean isValid = false;
+    private int counter = 5;
+
+    public Credentials credentials;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        eName = findViewById(R.id.usernameText);
+        ePassword = findViewById(R.id.passwordText);
+        eLogin = findViewById(R.id.btnLogin);
+        eAttemptsInfo = findViewById(R.id.tvAttemptsInfo);
+        registerButton = findViewById(R.id.buttonRegister);
+        eRememberMe = findViewById(R.id.cbRememberMe);
+        showPassword = findViewById(R.id.showPassword);
+
+        showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    ePassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    ePassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        credentials = new Credentials();
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
+        sharedPreferencesEditor = sharedPreferences.edit();
+
+        if(sharedPreferences != null){
+
+            Map<String, ?> preferencesMap = sharedPreferences.getAll();
+
+            if(preferencesMap.size() != 0){
+                credentials.loadCredentials(preferencesMap);
+            }
+
+            String savedUsername = sharedPreferences.getString("LastSavedUsername", "");
+            String savedPassword = sharedPreferences.getString("LastSavedPassword", "");
+
+            if(sharedPreferences.getBoolean("RememberMeCheckbox", false)){
+                eName.setText(savedUsername);
+                ePassword.setText(savedPassword);
+                eRememberMe.setChecked(true);
+            }
+        }
+
+        eRememberMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sharedPreferencesEditor.putBoolean("RememberMeCheckbox", eRememberMe.isChecked());
+                sharedPreferencesEditor.apply();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                if(inputName.isEmpty() || inputPassword.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter all the details correctly!", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    isValid = validate(inputName, inputPassword);
+
+                    if(!isValid){
+
+                        counter--;
+
+                        Toast.makeText(LoginActivity.this, "Incorrect credentials entered!", Toast.LENGTH_SHORT).show();
+
+                        eAttemptsInfo.setText("No. of attempts remaining: " + counter);
+
+                        if(counter == 0){
+                            eLogin.setEnabled(false);
+                        }
+
+                    }else{
+
+                        Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+
+                        sharedPreferencesEditor.putString("LastSavedUsername", inputName);
+                        sharedPreferencesEditor.putString("LastSavedPassword", inputPassword);
+
+                        sharedPreferencesEditor.apply();
+
+                        // Add the code to go to new activity
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+
+                }
+
+            }
+        });
+    }
+
+    private boolean validate(String name, String password){
+        return credentials.checkCredentials(name, password);
+    }
+}
