efd4f723a631188e969540479a81b40512d49c90
johannes <johannes.jokinen@metropolia.fi>
2021-02-26 15:31:44 +0200

    lisätty pohja exerciselistfragment- ja exercise-luokista

--- app/src/main/java/com/team1/hyteproject/HomeActivity.java
+++ app/src/main/java/com/team1/hyteproject/HomeActivity.java
@@ -2,6 +2,7 @@ package com.team1.hyteproject;
 
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.navigation.NavController;
--- app/src/main/java/com/team1/hyteproject/ui/exercise/ExerciseFragment.java
+++ app/src/main/java/com/team1/hyteproject/ui/exercise/ExerciseFragment.java
@@ -1,19 +1,26 @@
 package com.team1.hyteproject.ui.exercise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.team1.hyteproject.R;
+
+import java.lang.reflect.Array;
 
 public class ExerciseFragment extends Fragment {
 
@@ -31,30 +38,33 @@ public class ExerciseFragment extends Fragment {
         TextView textView = view.findViewById(R.id.textViewExercise);
         Log.d(TAG, "onCreateView: start.");
 
-        addExercise = view.findViewById(R.id.addExercise);
-        modifyExercise = view.findViewById(R.id.modifyExercise);
+            addExercise = view.findViewById(R.id.addExercise);
+            modifyExercise = view.findViewById(R.id.modifyExercise);
+            addExercise.setOnClickListener(new View.OnClickListener() {
 
-        addExercise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "addExercise clicked.");
-            }
-        });
 
-        modifyExercise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "modifyExercise clicked.");
-            }
-        });
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "addExercise clicked.");
+                }
+            });
 
-        exerciseViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return view;
+            modifyExercise.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "modifyExercise clicked.");
+                }
+            });
+
+            exerciseViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                @Override
+                public void onChanged(@Nullable String s) {
+                    textView.setText(s);
+                }
+
+
+
+            });
+            return view;
+        }
     }
-
-}
